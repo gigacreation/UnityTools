5d75188b182b38f05434f21f8955209bf6b01404
Gigacee <gigacee@gigacreation.jp>
2021-07-17 23:06:07 +0900

    Rigidbody2DExtensions を追加

---
+++ Assets/GigaceeTools/Core/Runtime/Extensions/Rigidbody2DExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace GigaceeTools
+{
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public static class Rigidbody2DExtensions
+    {
+        public static void MovePositionX(this Rigidbody2D self, float x)
+        {
+            self.MovePosition(new Vector2(x, self.position.y));
+        }
+
+        public static void MovePositionY(this Rigidbody2D self, float y)
+        {
+            self.MovePosition(new Vector2(self.position.x, y));
+        }
+    }
+}
