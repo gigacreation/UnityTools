7474cbcdd26e612c31fb0e8b96955509e6bff35b
Gigacee <gigacee@gigacreation.jp>
2021-08-28 16:39:36 +0900

    BoxCollider2D の Prefab のリバート機能を追加

--- Assets/GigaceeTools/General/Editor/MenuItems/Tools/PrefabReverting.cs
+++ Assets/GigaceeTools/General/Editor/MenuItems/Tools/PrefabReverting.cs
@@ -12,6 +12,7 @@ namespace GigaceeTools
         private const string RevertName = ToolsDirName + "Revert Name On Selected Prefabs";
         private const string RevertTransform = ToolsDirName + "Revert Transform On Selected Prefabs";
         private const string RevertRectTransform = ToolsDirName + "Revert RectTransform On Selected Prefabs";
+        private const string RevertBoxCollider2D = ToolsDirName + "Revert BoxCollider2D On Selected Prefabs";
         private const string RevertAll = ToolsDirName + "Revert All Properties On Selected Prefabs";
 
         private const InteractionMode U = InteractionMode.UserAction;
@@ -30,9 +31,9 @@ namespace GigaceeTools
         [MenuItem(RevertName, priority = CategoryPriority + 1)]
         public static void RevertNameOnSelectedPrefabs()
         {
-            foreach (GameObject obj in Selection.gameObjects)
+            foreach (GameObject go in Selection.gameObjects)
             {
-                var s = new SerializedObject(obj);
+                var s = new SerializedObject(go);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_Name"), U);
             }
         }
@@ -40,9 +41,9 @@ namespace GigaceeTools
         [MenuItem(RevertTransform, priority = CategoryPriority + 2)]
         public static void RevertTransformOnSelectedPrefabs()
         {
-            foreach (Transform trans in Selection.gameObjects.Select(obj => obj.transform))
+            foreach (Transform t in Selection.gameObjects.Select(obj => obj.transform))
             {
-                var s = new SerializedObject(trans);
+                var s = new SerializedObject(t);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_LocalPosition"), U);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_LocalRotation"), U);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_LocalScale"), U);
@@ -52,9 +53,9 @@ namespace GigaceeTools
         [MenuItem(RevertRectTransform, priority = CategoryPriority + 3)]
         public static void RevertRectTransformOnSelectedPrefabs()
         {
-            foreach (RectTransform rect in Selection.gameObjects.Select(obj => obj.transform as RectTransform))
+            foreach (RectTransform rt in Selection.gameObjects.Select(obj => obj.transform as RectTransform))
             {
-                var s = new SerializedObject(rect);
+                var s = new SerializedObject(rt);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_LocalPosition"), U);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_LocalRotation"), U);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_LocalScale"), U);
@@ -63,6 +64,24 @@ namespace GigaceeTools
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_AnchoredPosition"), U);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_SizeDelta"), U);
                 PrefabUtility.RevertPropertyOverride(s.FindProperty("m_Pivot"), U);
+            }
+        }
+
+        [MenuItem(RevertBoxCollider2D, priority = CategoryPriority + 4)]
+        public static void RevertBoxCollider2DOnSelectedPrefabs()
+        {
+            foreach (BoxCollider2D c in Selection.gameObjects.Select(obj => obj.GetComponent<BoxCollider2D>()))
+            {
+                var s = new SerializedObject(c);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_Density"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_Material"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_IsTrigger"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_UsedByEffector"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_UsedByComposite"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_AutoTiling"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_Offset"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_Size"), U);
+                PrefabUtility.RevertPropertyOverride(s.FindProperty("m_EdgeRadius"), U);
             }
         }
 
