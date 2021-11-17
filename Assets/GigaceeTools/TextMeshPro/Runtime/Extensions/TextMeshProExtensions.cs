ad1c4e1a91311a826cbcd8825f4403328cf01564
Gigacee <gigacee@gigacreation.jp>
2021-11-17 17:38:35 +0900

    TextMesh Pro の拡張メソッドを実装

---
+++ Assets/GigaceeTools/TextMeshPro/Runtime/Extensions/TextMeshProExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using TMPro;
+using UnityEngine;
+
+namespace GigaceeTools
+{
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static class TextMeshProExtensions
+    {
+        public static void SetAlpha(this TMP_Text self, float alpha)
+        {
+            Color color = self.color;
+            color.a = alpha;
+            self.color = color;
+        }
+    }
+}
