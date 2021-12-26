b4fd2e930934fa2c40a45cff24d7a695b1ad248d
Gigacee <gigacee@gigacreation.jp>
2021-12-26 19:00:05 +0900

    自身がすでに存在していたら自身を破棄するスクリプトを追加

---
+++ Assets/GigaceeTools/General/Runtime/DestroySelfIfAlreadyExists.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace GigaceeTools
+{
+    public class DestroySelfIfAlreadyExists : MonoBehaviour
+    {
+        [SerializeField] private Component _targetComponent;
+        [SerializeField] private DestroyTarget _destroyTarget;
+
+        private bool WillBeDestroyed { get; set; }
+
+        private void Awake()
+        {
+            if (_targetComponent == null)
+            {
+                return;
+            }
+
+            Object[] objectsOfSameTypeExceptSelf = FindObjectsOfType(_targetComponent.GetType(), true)
+                .Where(obj => (obj != _targetComponent)
+                              && obj is Component c
+                              && !c.GetComponent<DestroySelfIfAlreadyExists>().WillBeDestroyed)
+                .ToArray();
+
+            if (objectsOfSameTypeExceptSelf.Length == 0)
+            {
+                return;
+            }
+
+            WillBeDestroyed = true;
+
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            switch (_destroyTarget)
+            {
+                case DestroyTarget.Component:
+                    Destroy(_targetComponent);
+                    break;
+
+                case DestroyTarget.GameObject:
+                    Destroy(_targetComponent.gameObject);
+                    break;
+            }
+        }
+
+        private void OnValidate()
+        {
+            if ((_targetComponent == null) || (_targetComponent.gameObject == gameObject))
+            {
+                return;
+            }
+
+            Debug.LogWarning("自身と同じ GameObject にアタッチされたコンポーネントを指定してください。", gameObject);
+            _targetComponent = null;
+        }
+
+        private enum DestroyTarget
+        {
+            Component,
+            GameObject
+        }
+    }
+}
