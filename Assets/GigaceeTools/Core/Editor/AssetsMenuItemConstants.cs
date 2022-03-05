bfadf54be0ac4be4f5d0abb9051ae14ca16038e7
Gigacee <gigacee@gigacreation.jp>
2022-03-05 13:54:44 +0900

    Menu Item のパスを修正

--- Assets/GigaceeTools/Core/Editor/AssetsMenuItemConstants.cs
+++ Assets/GigaceeTools/Core/Editor/AssetsMenuItemConstants.cs
@@ -3,6 +3,6 @@
     public static class AssetsMenuItemConstants
     {
         public const int BasePriority = 2100000000;
-        public const string BasePath = "Assets/Gigacee Tools/";
+        public const string BasePath = "Assets/Gigacee/";
     }
 }
--- Assets/GigaceeTools/Core/Editor/ToolsMenuItemConstants.cs
+++ Assets/GigaceeTools/Core/Editor/ToolsMenuItemConstants.cs
@@ -3,7 +3,7 @@
     public static class ToolsMenuItemConstants
     {
         public const int BasePriority = -2100000000;
-        public const string BasePath = "Tools/Gigacee Tools/";
+        public const string BasePath = "Tools/Gigacee/";
         public const string CategoryPrefix = "-------- ";
         public const string CategorySuffix = " --------";
     }
