39f4d8ca3e2a4e8571d405ee5b1c9e02cc01f976
Gigacee <gigacee@gigacreation.jp>
2022-04-22 17:31:23 +0900

    Common Utility を追加

---
+++ Assets/GigaceeTools/UniTask/Runtime/ICommonWaitable.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace GigaceeTools
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.Members)]
+    public interface ICommonWaitable
+    {
+        /// <summary>
+        /// タスクが完了になるまで待機し、呼ばれてから完了するまでに掛かった時間を返します。
+        /// </summary>
+        /// <param name="ct">キャンセルトークン。</param>
+        /// <returns>完了を待機するタスク。</returns>
+        UniTask<float> WaitForCompletionAsync(CancellationToken ct = default);
+    }
+}
