4d0c88a29178784542c3cfbad9df231fc3687152
Gigacee <gigacee@gigacreation.jp>
2022-09-22 23:56:15 +0900

    CanvasGropuExtensions で interactable も変更するようにする

--- Assets/GigaceeTools/Ui/Runtime/Extensions/CanvasGroupExtensions.cs
+++ Assets/GigaceeTools/Ui/Runtime/Extensions/CanvasGroupExtensions.cs
@@ -9,12 +9,14 @@ namespace GigaceeTools
         public static void Activate(this CanvasGroup self)
         {
             self.alpha = 1f;
+            self.interactable = true;
             self.blocksRaycasts = true;
         }
 
         public static void Deactivate(this CanvasGroup self)
         {
             self.alpha = 0f;
+            self.interactable = false;
             self.blocksRaycasts = false;
         }
     }
