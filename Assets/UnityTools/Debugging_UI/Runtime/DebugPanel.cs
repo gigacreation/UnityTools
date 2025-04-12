7a4d69d954fdddbe61a7a4bad27be5b741732390
Gigacee <gigacee@gigacreation.jp>
2025-04-12 22:08:59 +0900

    非排他のデバッグパネルを実装

---
+++ Assets/UnityTools/Debugging_UI/Runtime/DebugPanel.cs
@@ -0,0 +1,83 @@
+using GigaCreation.Tools.Debugging.Core;
+using GigaCreation.Tools.Service;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GigaCreation.Tools.Debugging.Ui
+{
+    /// <summary>
+    /// デバッグモード中、表示・非表示を切り替えることができるパネルです。
+    /// </summary>
+    public class DebugPanel : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _contents;
+
+        [Space]
+        [SerializeField] private CanvasGroup _showButtonCanvasGroup;
+        [SerializeField] private Button _showButton;
+        [SerializeField] private CanvasGroup _hideButtonCanvasGroup;
+        [SerializeField] private Button _hideButton;
+
+        private void Start()
+        {
+            if (!ServiceLocator.TryGet(out IDebugManager debugManager))
+            {
+                return;
+            }
+
+            _showButton
+                .OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    ShowContent();
+                })
+                .AddTo(this);
+
+            _hideButton
+                .OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    HideContent();
+                })
+                .AddTo(this);
+
+            debugManager
+                .IsDebugMode
+                .Where(static x => x)
+                .Subscribe(_ =>
+                {
+                    HideContent();
+                })
+                .AddTo(this);
+        }
+
+        protected virtual void ShowContent()
+        {
+            foreach (Transform content in _contents)
+            {
+                content.localScale = Vector3.one;
+            }
+
+            _showButtonCanvasGroup.alpha = 0f;
+            _showButtonCanvasGroup.blocksRaycasts = false;
+
+            _hideButtonCanvasGroup.alpha = 1f;
+            _hideButtonCanvasGroup.blocksRaycasts = true;
+        }
+
+        protected virtual void HideContent()
+        {
+            foreach (Transform content in _contents)
+            {
+                content.localScale = Vector3.zero;
+            }
+
+            _showButtonCanvasGroup.alpha = 1f;
+            _showButtonCanvasGroup.blocksRaycasts = true;
+
+            _hideButtonCanvasGroup.alpha = 0f;
+            _hideButtonCanvasGroup.blocksRaycasts = false;
+        }
+    }
+}
---
+++ Assets/UnityTools/Debugging_UI/Runtime/DebugPanelExclusive.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace GigaCreation.Tools.Debugging.Ui
+{
+    /// <summary>
+    /// デバッグモード中、表示・非表示を切り替えることができるパネルです。
+    /// パネルが複数ある場合、一つのパネルを表示すると他のパネルは自動で非表示になります。
+    /// </summary>
+    public class DebugPanelExclusive : DebugPanel
+    {
+        private Transform[] _otherDebugPanels;
+
+        private bool _isQuitting;
+
+        private Transform[] OtherDebugPanels
+            => _otherDebugPanels ??= FindObjectsByType<DebugPanelExclusive>(FindObjectsSortMode.None)
+                .Where(panel => panel != this)
+                .Select(static panel => panel.transform)
+                .ToArray();
+
+        private void OnDestroy()
+        {
+            if (_isQuitting)
+            {
+                return;
+            }
+
+            foreach (Transform panel in OtherDebugPanels)
+            {
+                panel.localScale = Vector3.one;
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            _isQuitting = true;
+        }
+
+        protected override void ShowContent()
+        {
+            base.ShowContent();
+
+            foreach (Transform panel in OtherDebugPanels)
+            {
+                panel.localScale = Vector3.zero;
+            }
+        }
+
+        protected override void HideContent()
+        {
+            base.HideContent();
+
+            foreach (Transform panel in OtherDebugPanels)
+            {
+                panel.localScale = Vector3.one;
+            }
+        }
+    }
+}
