ee7a48eeff477fb646a1fdbdaf3c27fdc54d56d0
Gigacee <gigacee@gigacreation.jp>
2025-04-13 12:50:25 +0900

    解像度デバッグテキスト設定のテキストを修正

--- Assets/UnityTools/Debugging_TextDisplays/Runtime/DebugTextPreferenceResolution.cs
+++ Assets/UnityTools/Debugging_TextDisplays/Runtime/DebugTextPreferenceResolution.cs
@@ -4,6 +4,16 @@ namespace GigaCreation.Tools.Debugging.TextDisplays
 {
     public class DebugTextPreferenceResolution : DebugTextPreferenceBase
     {
-        protected override string LabelText => $"Current Resolution: {Screen.currentResolution}";
+        protected override string LabelText
+        {
+            get
+            {
+                int width = Screen.currentResolution.width;
+                int height = Screen.currentResolution.height;
+                double refreshRate = Screen.currentResolution.refreshRateRatio.value;
+
+                return $"Resolution: {width} x {height} @ {refreshRate:0.00}Hz";
+            }
+        }
     }
 }
